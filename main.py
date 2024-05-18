a7b94da081974c4359793089ab7dd6193d302cd1
kr1st4ps <kristaps.almanis14@gmail.com>
2024-05-18 12:35:24 +0300

    new flat court image

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ RESULT_PATH = os.path.join("resources", "runs")
 FRAME_COUNTER = 1
 
 #   Opens video
-INPUT_VIDEO = os.path.join(VIDEO_FOLDER, "test_video.mp4")
+INPUT_VIDEO = os.path.join(VIDEO_FOLDER, "test_video4.mp4")
 filename = os.path.splitext(os.path.basename(INPUT_VIDEO))[0]
 court_kp_file_path = os.path.join(KP_FOLDER, filename + ".json")
 sb_kp_file_path = os.path.join(KP_FOLDER, filename + "_sb" + ".json")
